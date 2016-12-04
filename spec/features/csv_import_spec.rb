7aadb4334d84f2273235e98da97e4664ce9e77ef
garedean <garrettdolson@gmail.com>
2016-12-04 08:44:19 -0800

    Add feature spec verifying flash for malformed CSV

--- spec/features/csv_import_spec.rb
+++ spec/features/csv_import_spec.rb
@@ -24,4 +24,14 @@ feature 'CSV Import' do
 
     expect(page).to have_content 'CSV imported'
   end
+
+  scenario 'shows error message when malformed CSV provided' do
+    visit root_path
+
+    file_path = Rails.root + 'spec/fixtures/malformed_sales_test_data.csv'
+    attach_file('csv_file', file_path)
+    click_button 'Import'
+
+    expect(page).to have_content 'Error importing CSV. Check file formatting'
+  end
 end
