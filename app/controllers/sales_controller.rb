99640298e92db6d3b02ae27963c53128962f0c88
garedean <garrettdolson@gmail.com>
2016-12-03 23:54:22 -0800

    Refactor sales controller

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,12 +1,24 @@
 class SalesController < ApplicationController
   def csv_import
-    csv_file = params[:csv_file]
-
     if csv_file
       Sale.csv_import(csv_file)
-      redirect_to root_path, notice: 'CSV imported'
+      set_flash_notice('CSV imported')
     else
-      redirect_to root_path, notice: 'File missing'
+      set_flash_notice('File missing')
     end
+  rescue CSV::MalformedCSVError
+    set_flash_notice('Error importing CSV. Check file formatting')
+  ensure
+    redirect_to root_path
+  end
+
+  private
+
+  def csv_file
+    params[:csv_file]
+  end
+
+  def set_flash_notice(message)
+    flash[:notice] = message
   end
 end
